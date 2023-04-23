b635e9920cfb9f919f24a5f0ae79906fa9a5c191
Amir <amirmohammad7284@gmail.com>
2023-04-23 21:43:28 +0330

    Updated client.py

--- client.py
+++ client.py
@@ -1,4 +1,3 @@
-import threading
 import socket
 
 
@@ -6,4 +5,23 @@ HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = "127.0.0.1"
+IP = socket.gethostbyname(socket.gethostname())
+ADDR = (IP, PORT)
+
+# creating the client side of the socket
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+
+send("Hello World!")
+send(DISCONNECT_MESSAGE)
