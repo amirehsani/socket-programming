e28e6190273ec0653737ae5734c4563934131809
Amir <amirmohammad7284@gmail.com>
2023-05-23 10:34:01 +0330

    Fixed error with sending multiple packets

--- sockets/client.py
+++ sockets/client.py
@@ -1,6 +1,5 @@
 import socket
 
-
 HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
@@ -11,6 +10,7 @@ ADDR = (IP, PORT)
 # creating the client side of the sockets
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
+client.setblocking(False)  # Set socket to non-blocking mode
 
 
 def send(msg):
@@ -20,16 +20,21 @@ def send(msg):
     send_length += b' ' * (HEADER - len(send_length))
     client.send(send_length)
     client.send(message)
-    print(client.recv(2048).decode(FORMAT))
 
 
 while True:
-
     packet = input()
 
-    if str(packet) != 'disconnect':
+    if packet != 'disconnect':
         send(packet)
 
     else:
         send(DISCONNECT_MESSAGE)
         break
+
+    try:
+        response = client.recv(2048).decode(FORMAT)
+        if response:
+            print(response)
+    except socket.error:
+        pass
